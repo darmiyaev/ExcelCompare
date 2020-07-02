0e1afbed91988605656328867abc24655217263f
darmiyaev <40632841+darmiyaev@users.noreply.github.com>
2020-07-02 13:00:49 -0400

    Create ExcelComparesetup.py

---
+++ ExcelComparesetup.py
@@ -0,0 +1,6 @@
+from cx_Freeze import setup, Executable 
+  
+setup(name = "ExcelCompare" , 
+      version = "0.1" , 
+      description = "Compare 2 excel files easily and quickly!" , 
+      executables = [Executable("C:/Users/Daria/AppData/Local/Programs/Python/Python38-32/Excelcompare2.py")]) 
